deafefb9a056d69d01e7128d2b5ab73b2d07d7bb
MskTmi <1226594277@qq.com>
2024-01-03 18:21:00 +0800

    feat：优化mikoto展示方法

--- source/js/plugins/start.js
+++ source/js/plugins/start.js
@@ -1,15 +1,19 @@
 // 给主页添加scroll事件的监听器
-if (window.location.pathname == "/") {
-    window.addEventListener("scroll", function () {
-        // 获取当前滚动条的位置
-        var scrollPos = window.scrollY;
-        // 判断是否满足显示或隐藏的条件
-        if (scrollPos >= 400) {
-            // 如果滚动条位置大于等于400显示live2d
-            document.getElementById("live2d-widget").style.display = "block";
-        } else {
-            // 如果滚动条位置小于400隐藏live2d
-            document.getElementById("live2d-widget").style.display = "none";
-        }
-    });
+window.onload = function () {
+    if (window.location.pathname == "/") {
+        // 在封面图上隐藏
+        document.getElementById("live2d-widget").style.bottom = "-400px";
+        window.addEventListener("scroll", function () {
+            // 获取当前滚动条的位置
+            var scrollPos = window.scrollY;
+            // 判断是否满足显示或隐藏的条件
+            if (scrollPos >= 400) {
+                // 如果滚动条位置大于等于400显示live2d
+                document.getElementById("live2d-widget").style.bottom = "0px";
+            } else {
+                // 如果滚动条位置小于400隐藏live2d
+                document.getElementById("live2d-widget").style.bottom = scrollPos - 400 + "px";
+            }
+        });
+    }
 }
