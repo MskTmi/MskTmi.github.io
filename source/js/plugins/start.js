5f732d5641ba9b34f71028a3d41d4e9aa99ce73a
MskTmi <1226594277@qq.com>
2024-01-25 11:44:00 +0800

    fix：修复只有窗口发生变化时才隐藏live2d

--- source/js/plugins/start.js
+++ source/js/plugins/start.js
@@ -1,33 +1,40 @@
 // 给主页添加scroll事件的监听器
+
 window.onload = function () {
+    window.mikoto = document.getElementById("live2d-widget");
+
+    window.mikoto.style.opacity = 1;
     if (window.location.pathname == "/") {
         // 在封面图上隐藏
-        document.getElementById("live2d-widget").style.bottom = "-400px";
+        window.mikoto.style.bottom = "-400px";
         window.addEventListener("scroll", function () {
             // 获取当前滚动条的位置
             var scrollPos = window.scrollY;
             // 随着滚动条下移live2d
             if (scrollPos >= 400) {
-                // 如果滚动条位置大于等于400显示live2d
-                document.getElementById("live2d-widget").style.bottom = "0px";
+                // 如果滚动条位置大于等于400的位置显示live2d
+                window.mikoto.style.bottom = "0px";
             } else {
                 // 如果滚动条位置小于400隐藏live2d
-                document.getElementById("live2d-widget").style.bottom = scrollPos - 400 + "px";
+                window.mikoto.style.bottom = scrollPos - 400 + "px";
             }
         });
     }
-    document.getElementById("live2d-widget").style.opacity = 1;
 
+    //窗口小于1650不显示mikoto
+    displayLive2d(1650);
     // 监听窗口大小变化事件
     window.addEventListener("resize", function () {
-        var width = window.innerWidth;
-
-        // 如果页面宽度小于1650px，隐藏元素
-        if (width < 1650) {
-            document.getElementById("live2d-widget").style.display = "none";
-        } else {
-            // 否则，显示元素
-            document.getElementById("live2d-widget").style.display = "block";
-        }
+        displayLive2d(1650);
     });
+}
+
+function displayLive2d(width) {
+    //是否小于指定窗口大小
+    if (window.innerWidth < width) {
+        window.mikoto.style.display = "none";
+    } else {
+        // 否则，显示元素
+        window.mikoto.style.display = "block";
+    }
 }
